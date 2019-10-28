35b979fbdecd8954aa074e6c92f2c08d19fe417b
horothesun <horothesun@gmail.com>
2019-10-28 11:17:42 +0000

    Cleanup

--- unflattened.test.js
+++ unflattened.test.js
@@ -1,26 +1,26 @@
 const { unflattened } = require('./unflattened');
 
-test("returns [] from [] array and 0 elementsPerGroup", () => {
+test("returns [] from [] and 0 group size", () => {
     const result = unflattened([], 0)
     expect(result).toStrictEqual([])
 })
 
-test("returns [] from [] array and 1 elementsPerGroup", () => {
+test("returns [] from [] and 1 group size", () => {
     const result = unflattened([], 1)
     expect(result).toStrictEqual([])
 })
 
-test("returns [['a']] from ['a'] array and 3 elementsPerGroup", () => {
+test("returns [['a']] from ['a'] and 3 group size", () => {
     const result = unflattened(['a'], 3)
     expect(result).toStrictEqual([['a']])
 })
 
-test("returns [[1, 2], [3, 4]] from [1, 2, 3, 4] array and 2 elementsPerGroup", () => {
+test("returns [[1, 2], [3, 4]] from [1, 2, 3, 4] and 2 group size", () => {
     const result = unflattened([1, 2, 3, 4], 2)
     expect(result).toStrictEqual([[1, 2], [3, 4]])
 })
 
-test("returns [[1, 2], [3, 4], [5]] from [1, 2, 3, 4, 5] array and 2 elementsPerGroup", () => {
+test("returns [[1, 2], [3, 4], [5]] from [1, 2, 3, 4, 5] and 2 group size", () => {
     const result = unflattened([1, 2, 3, 4, 5], 2)
     expect(result).toStrictEqual([[1, 2], [3, 4], [5]])
 })
